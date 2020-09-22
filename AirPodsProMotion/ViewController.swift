247da0e985279239662829bdbbe46b8644f6aeab
tukuyo <ht15a053@oecu.jp>
2020-09-23 00:34:58 +0900

    Add: README.md

--- AirPodsProMotion/ViewController.swift
+++ AirPodsProMotion/ViewController.swift
@@ -19,20 +19,19 @@ class ViewController: UIViewController {
         let APP = CMHeadphoneMotionManager()
 
         guard APP.isDeviceMotionAvailable else { return }
-        APP.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {[weak self] (motion,error)  in
-            guard let _ = motion, error == nil else { return }
+        APP.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {[weak self] _,_  in
             self?.printData(APP.deviceMotion!)
         })
     }
     
     func printData(_ data: CMDeviceMotion) {
         print(data)
-        print(data.attitude)            // 姿勢 pitch, roll, yaw
-        print(data.gravity)             // 重力加速度
-        print(data.rotationRate)        // 角速度
-        print(data.userAcceleration)    // 加速度
-        print(data.magneticField)       // 磁気フィールド　磁気ベクトルを返す
-        print(data.heading)             // 方位角
+//        print(data.attitude)            // 姿勢 pitch, roll, yaw
+//        print(data.gravity)             // 重力加速度
+//        print(data.rotationRate)        // 角速度
+//        print(data.userAcceleration)    // 加速度
+//        print(data.magneticField)       // 磁気フィールド　磁気ベクトルを返す
+//        print(data.heading)             // 方位角
         
         self.textView.text = """
             姿勢:
---
+++ README.md
@@ -0,0 +1,19 @@
+# AirPods Pro Motion Sample
+
+AirPods Pro のモーションセンサの値を取得するサンプルプロジェクト
+
+## Requirements 必要事項
+ - Xcode: 12.0
+ - iOS  : 14
+ - 機材
+ - iPhone, iPad
+ - AirPods Pro
+  
+## Reference
+ - [Apple Developer Documentation](https://developer.apple.com/documentation/coremotion/cmheadphonemotionmanager)
+
+
+## Author
+ Yoshio Tsukuda  
+ - [HP](https://tukuyo.net/)
+ - [Twitter](https://twitter.com/tukutuku_tukuyo)
