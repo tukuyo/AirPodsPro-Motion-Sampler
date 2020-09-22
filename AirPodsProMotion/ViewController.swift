f8f0f7bdb73612cb5efdb1fb6816a64ea4f5ec08
tukuyo <ht15a053@oecu.jp>
2020-09-23 00:18:41 +0900

    Add: Sample Project

--- AirPodsProMotion/ViewController.swift
+++ AirPodsProMotion/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //AirPods Pro => APP :)
+        let APP = CMHeadphoneMotionManager()
+
+        guard APP.isDeviceMotionAvailable else { return }
+        APP.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {[weak self] (motion,error)  in
+            guard let _ = motion, error == nil else { return }
+            self?.printData(APP.deviceMotion!)
+        })
+    }
+    
+    func printData(_ data: CMDeviceMotion) {
+        print(data)
+        print(data.attitude)            // 姿勢 pitch, roll, yaw
+        print(data.gravity)             // 重力加速度
+        print(data.rotationRate)        // 角速度
+        print(data.userAcceleration)    // 加速度
+        print(data.magneticField)       // 磁気フィールド　磁気ベクトルを返す
+        print(data.heading)             // 方位角
+        
+        self.textView.text = """
+            姿勢:
+                pitch: \(data.attitude.pitch)
+                roll: \(data.attitude.roll)
+                yaw: \(data.attitude.yaw)
+            重力加速度:
+                x: \(data.gravity.x)
+                y: \(data.gravity.y)
+                z: \(data.gravity.z)
+            角速度:
+                x: \(data.rotationRate.x)
+                y: \(data.rotationRate.y)
+                z: \(data.rotationRate.z)
+            加速度:
+                x: \(data.userAcceleration.x)
+                y: \(data.userAcceleration.y)
+                z: \(data.userAcceleration.z)
+            磁気フィールド:
+                field: \(data.magneticField.field)
+                accuracy: \(data.magneticField.accuracy)
+            方位角:
+                \(data.heading)
+            """
     }
 
-
 }
-
